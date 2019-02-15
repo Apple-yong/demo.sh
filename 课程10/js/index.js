52e22cde6a7825e316418a422874507ba47f80e9
Apple-yong <748840808@qq.com>
2019-02-15 17:51:47 +0800

    如何解决问题17.07

--- 课程10/js/index.js
+++ 课程10/js/index.js
@@ -13,3 +13,21 @@ portfolio3.onclick = function () {
 window.onscroll = function () {
     window.scrollY > 0 ? (topNavBar.classList.add('sticky')):(topNavBar.classList.remove('sticky'))
 }
+
+let liTags = document.getElementsByClassName('menuTigger')
+for(let i=0; i<liTags.length; i++){
+    liTags[i].onmouseenter = function (x) {
+        // console.log(this.children[1])
+        let li = x.currentTarget
+        let brother = li.getElementsByTagName('ul')[0]
+        // while(brother.tagName !== 'UL'){
+        //     brother = brother.nextSibling
+        // }
+        brother.classList.add('active')
+    }
+    liTags[i].onmouseleave = function (x) {
+        let li = x.currentTarget
+        let brother = li.getElementsByTagName('ul')[0]
+        brother.classList.remove('active')
+    }
+}
