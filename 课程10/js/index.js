d0f514ca30baae722e58e15fef477d1291c4896b
Apple-yong <748840808@qq.com>
2019-02-21 17:17:25 +0800

    原生JS缓慢移动，主要是移动25次，给成一种错觉

--- 课程10/js/index.js
+++ 课程10/js/index.js
@@ -33,9 +33,22 @@ let aTags = document.querySelectorAll('nav.menu > ul > li > a')
 for(let i=0; i<aTags.length; i++){
     aTags[i].onclick = function (x) {
         x.preventDefault();//取消默认的锚点滚动
-        let href = this.getAttribute('href')
-        let element = document.querySelector(href)
-        let top = element.offsetTop//获取该div到定点的距离
-        window.scrollTo(0, top-80)
+        // let href = this.getAttribute('href')
+        // let element = document.querySelector(href)
+        let top = document.querySelector(this.getAttribute('href')).offsetTop//获取该div到定点的距离
+        let n = 25 //动25次
+        let duration = 500 / n //多少时间动一次
+        let currentTop = window.scrollY //当前高度
+        let targetTop = top - 80 //目标top
+        let distance = (targetTop - currentTop)/ n //每次动多少
+        let i = 0
+        let id = setInterval(() => {
+            if(i===n){ //等于25次时停止
+                window.clearInterval(id)
+                return
+            }
+            i = i + 1
+            window.scrollTo(0, currentTop + distance * i)
+        }, duration)  
     }
 }
