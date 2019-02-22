192cb268d6c0f9c1b95016967fc7869157bf6eae
Apple-yong <748840808@qq.com>
2019-02-22 16:19:24 +0800

    导航栏随滚动高亮

--- 课程10/js/index.js
+++ 课程10/js/index.js
@@ -12,6 +12,21 @@ portfolio3.onclick = function () {
 }
 window.onscroll = function () {
     window.scrollY > 0 ? (topNavBar.classList.add('sticky')):(topNavBar.classList.remove('sticky'))
+    let specialTags = document.querySelectorAll('[data-x]')
+    let minIndex = 0
+    for(let i=0;i<specialTags.length;i++){
+        if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+            minIndex = i
+        }
+    }
+    let id = specialTags[minIndex].id
+    let a = document.querySelector('a[href="#'+ id +'"]')
+    let li = a.parentNode
+    let brothersAndme = li.parentNode.children
+    for(let i=0;i<brothersAndme.length;i++){
+        brothersAndme[i].classList.remove('highlight')
+    }
+    li.classList.add('highlight')
 }
 
 let liTags = document.querySelectorAll('nav.menu > ul > li')
