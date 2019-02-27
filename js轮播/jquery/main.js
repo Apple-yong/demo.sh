dc8261d559d7c11199b912fef143d28340700e63
Apple-yong <748840808@qq.com>
2019-02-27 16:01:00 +0800

    第一种简便实现方法

---
+++ js轮播/jquery/main.js
@@ -0,0 +1,15 @@
+$(p1).click(function(){
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+});
+$(p2).click(function(){
+    $(images).css({
+        transform: 'translateX(-300px)'
+    })
+});
+$(p3).click(function(){
+    $(images).css({
+        transform: 'translateX(-600px)'
+    })
+});
