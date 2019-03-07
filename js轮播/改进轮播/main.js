cf701aabcd554a6febbfa3ce4e8c73584c947015
Apple-yong <748840808@qq.com>
2019-03-07 18:07:24 +0800

    最终优化

--- js轮播/改进轮播/main.js
+++ js轮播/改进轮播/main.js
@@ -5,38 +5,57 @@ let current = 0
 makeSlides()
 $("#slides").css({transform:'translateX(-300px)'})
 bindEvents()
+$(next).on('click', function(){
+    goToSlide(current+1)
+})
+$(previous).on('click', function(){
+    goToSlide(current-1)
+})
 
-function bindEvents() {  
-    $buttons.eq(0).click(function(){
-        if(current == 2){
-            $("#slides").css({transform:'translateX(-1200px)'})//假的
-            .one('transitionend', function () {
-                $("#slides").hide().offset()
-                $("#slides").css({transform:'translateX(-300px)'}).show()
-            })
-        }else{
-            $("#slides").css({transform:'translateX(-300px)'})
-        }
-        current = 0
-    })
-    $buttons.eq(1).click(function(){
-        $("#slides").css({transform:'translateX(-600px)'})
-        current = 1
-    })
-    $buttons.eq(2).click(function(){
-        if(current == 0){
-            $("#slides").css({transform:'translateX(0px)'})//假的
-            .one('transitionend', function () {
-                $("#slides").hide().offset()
-                $("#slides").css({transform:'translateX(-900px)'}).show()
-            })
-        }else{
-            $("#slides").css({transform:'translateX(-900px)'})
-            current = 2
-        }
-        
+let timer = setInterval(function(){
+    goToSlide(current+1)
+},3000)
+$('.container').on('mouseenter', function(){
+    window.clearInterval(timer)
+}).on('mouseleave', function(){
+    timer = setInterval(function(){
+        goToSlide(current+1)
+    },3000)
+})
+
+function bindEvents() {
+    $buttons.click(function(){
+        let index = $(this).index()
+        goToSlide(index)
     })
 }
+
+function goToSlide(index) {
+    if(index > $buttons.length-1){
+        index = 0
+      }else if(index <0){
+        index = $buttons.length - 1
+    }  
+    if(current === $buttons.length -1 && index === 0){
+        //最后一张到第一张
+        $("#slides").css({transform:`translateX(${-($buttons.length + 1) * 300}px)`})//假的
+            .one('transitionend', function () {
+                $("#slides").hide().offset()
+                $("#slides").css({transform:`translateX(${-(index+1)*300}px)`}).show()
+            })
+    }else if(current === 0 && index === $buttons.length - 1){
+        // 第一张到最后一张
+        $("#slides").css({transform:`translateX(0px)`})
+                .one('transitionend', function(){
+                $("#slides").hide().offset()
+                $("#slides").css({transform:`translateX(${-(index+1)*300}px)`}).show()
+            })
+    }else{
+        $("#slides").css({transform:`translateX(${- (index + 1) * 300}px)`})
+    }
+    current = index
+}
+
 
 
 function makeSlides() {  
