674866cdefc5b3723ea869dc4cf43e15dac24549
Apple-yong <748840808@qq.com>
2019-03-08 17:59:13 +0800

    苹果轮播

---
+++ js轮播/苹果轮播/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	/* 自动计算轮播视图宽度 */
+	var totWidth=0
+	var positions = new Array()
+	$('#slides .slide').each(function(i){
+		positions[i]= totWidth
+		totWidth = $(this).width() + totWidth
+	})
+	$('#slides').width(totWidth)
+
+	
+
+	$('#menu ul li a').click(function(e,keepScroll){
+			$('li.menuItem').removeClass('act').addClass('inact')
+			$(this).parent().addClass('act')
+			var pos = $(this).parent().prevAll('.menuItem').length//获取第几个轮播按钮
+			$('#slides').animate({marginLeft:-positions[pos]+'px'},450)//切换及切换速度
+			
+			e.preventDefault()
+			// 点击图标，停止自动滚动
+			if(!keepScroll) clearInterval(itvl)
+	})
+	
+	$('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact')
+	/* On page load, mark the first thumbnail as active */
+	
+	
+	
+	//自动轮播
+	var current=1
+	function autoAdvance(){
+		if(current==-1) return false
+		$('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click',[true])
+		current++
+	}
+
+	//自动轮播秒数
+	var changeEvery = 10
+	var itvl = setInterval(function(){autoAdvance()},changeEvery*1000)
+})
