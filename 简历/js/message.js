20b374d6cce8ede6d4fac6b7c2db7db29e7ebe8f
Apple-yong <748840808@qq.com>
2019-06-18 17:59:09 +0800

    回退测试

--- 简历/js/message.js
+++ 简历/js/message.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 !function () {
     var model = {
         // 获取数据
@@ -76,3 +77,62 @@
 
 
 }.call()
+=======
+var APP_ID = 'fICGXh1f61rlfh1Nm1fcPaHb-gzGzoHsz';
+var APP_KEY = 'm7cUoDEz8GOhedE0Lw8l44qc';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+
+var query = new AV.Query('Message');
+query.find()
+  .then(
+    function (messages) {
+      let array = messages.map((item)=> item.attributes )
+      array.forEach((item)=>{
+        let li = document.createElement('li')
+        li.innerText = `${item.name}: ${item.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+      })
+    } 
+  )
+
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit', function(e){
+  e.preventDefault()
+  let content = myForm.querySelector('input[name=content]').value
+  let name = myForm.querySelector('input[name=name]').value
+  var Message = AV.Object.extend('Message');
+  var message = new Message();
+  message.save({
+    'name': name,
+    'content': content
+  }).then(function(object) {
+    let li = document.createElement('li')
+    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+    let messageList = document.querySelector('#messageList')
+    messageList.appendChild(li)
+    myForm.querySelector('input[name=content]').value = ''
+    console.log(object)
+  })
+})
+
+/*
+// 创建 TestObject 表
+var X = AV.Object.extend('Frank2');
+// 在表中创建一行数据
+var o = new X();
+// 数据内容是 words: 'Hello World!' 保存
+// 如果保存成功，则运行 alert('')
+o.save({
+  xxxxxxxxxx: 'Fuck World!'
+}).then(function() {
+  console.log(arguments[0])
+})
+*/
+>>>>>>> parent of 8b5a24a... no message
