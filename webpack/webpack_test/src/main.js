4139924ca450c8c03f3265680895b4f5248a0025
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-17 22:39:24 +0800

    webpack端口、自动加载、自动打包等设置

--- webpack/webpack_test/src/main.js
+++ webpack/webpack_test/src/main.js
@@ -2,7 +2,7 @@
 
 import $ from 'jquery'
 $(function () {
-    $('li:odd').css('backgroundColor', 'black')
+    $('li:odd').css('backgroundColor', 'red')
     $('li:even').css('backgroundColor', function () {
         return '#' + 'D97634'
     })
