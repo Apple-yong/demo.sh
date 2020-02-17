679990c738ee6fcadc10030404bcb071c08cf26c
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-17 23:18:41 +0800

    处理css文件

--- webpack/webpack_test/src/main.js
+++ webpack/webpack_test/src/main.js
@@ -1,6 +1,7 @@
 // 项目JS入口文件
 
 import $ from 'jquery'
+import './css/index.css'
 $(function () {
     $('li:odd').css('backgroundColor', 'black')
     $('li:even').css('backgroundColor', function () {
--- webpack/webpack_test/webpack.config.js
+++ webpack/webpack_test/webpack.config.js
@@ -10,10 +10,15 @@ module.exports = {
         path: path.join(__dirname, './dist'),  //输出目录
         filename: 'bundle.js'
     },
-    plugins: [
+    plugins: [ //配置插件的节点
         new htmlWebpackPlugin({   //创建一个在内存中生成HTML页面的插件
             template: path.join(__dirname, './src/index.html'), //指定模板页面
             filename: 'index.html'
         })
-    ]
+    ],
+    module: {  //用于配置所有第三方模块加载器
+        rules: [
+            { test: /\.css$/, use:['style-loader', 'css-loader'] }  //匹配.css结尾的文件,使用这两个插件处理
+        ]
+    }
 }
