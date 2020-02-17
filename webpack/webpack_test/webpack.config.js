91c192b4576a1f51044d0591f545d14499e899f0
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-17 23:01:23 +0800

    安装htmlWebpackPlugin，自动引用js文件

--- webpack/webpack_test/webpack.config.js
+++ webpack/webpack_test/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 // webpack配置文件
 module.exports = {
     //入口路径
@@ -7,5 +9,11 @@ module.exports = {
     output: {
         path: path.join(__dirname, './dist'),  //输出目录
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({   //创建一个在内存中生成HTML页面的插件
+            template: path.join(__dirname, './src/index.html'), //指定模板页面
+            filename: 'index.html'
+        })
+    ]
 }
