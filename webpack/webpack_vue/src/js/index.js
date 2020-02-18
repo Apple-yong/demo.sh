392bc93eaab1b8d26de4a5d4276f8a1d4ecf00b1
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-18 22:32:37 +0800

    export

---
+++ webpack/webpack_vue/src/js/index.js
@@ -0,0 +1,8 @@
+var info = {
+    name: '21',
+    age: '23'
+}
+
+export var title = "历史"
+export var book = "语文"
+export default info
--- webpack/webpack_vue/src/main.js
+++ webpack/webpack_vue/src/main.js
@@ -1,6 +1,7 @@
 // 项目JS入口文件
 import Vue from "vue"
 import login from "./login.vue"
+import info,{ title, book } from "../src/js/index.js"
 
 var vm = new Vue({
     el: "#app",
@@ -9,3 +10,7 @@ var vm = new Vue({
     },
     render: params => params(login)
 })
+
+console.log(info)
+console.log(title)
+console.log(book)
