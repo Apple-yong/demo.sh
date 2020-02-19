76019e820ca9c13291c36f96f3d0501ca8be6a4e
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-19 21:45:40 +0800

    注释

--- webpack/webpack_router/src/main.js
+++ webpack/webpack_router/src/main.js
@@ -1,10 +1,12 @@
 // 项目JS入口文件
 import Vue from "vue"
-import VueRouter from "vue-router"
 import App from "./App.vue"
-import router from "./router.js"
-Vue.use(VueRouter)
 
+//引入vue-router
+import VueRouter from "vue-router"
+Vue.use(VueRouter)
+//引入自己封装的router路由
+import router from "./router.js"
 
 var vm = new Vue({
     el: "#app",
