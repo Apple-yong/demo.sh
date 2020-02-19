e00aa7ab68c30b821e713f849e2c42de5c196e2e
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-19 17:24:53 +0800

    子路由

--- webpack/webpack_router/src/main.js
+++ webpack/webpack_router/src/main.js
@@ -2,22 +2,33 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 
-Vue.use(VueRouter)
-
 // 导入组件
-import login from "./login.vue"
-import Account from "./main/Account.vue"
-import GoodsList from "./main/GoodsList.vue"
+import App from "./App.vue"
+import account from "./main/Account.vue"
+import goodslist from "./main/GoodsList.vue"
+
+// 导入子组件
+import login from './subcom/login.vue'
+import register from './subcom/register.vue'
+
+Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes: [
-        { path: '/account', component: Account },
-        { path: '/goodsList', component: GoodsList }
+        {
+            path: '/account',
+            component: account,
+            children: [
+                { path: 'login', component: login },
+                { path: 'register', component: register }
+            ]
+        },
+        { path: '/goodslist', component: goodslist }
     ]
 })
 
 var vm = new Vue({
     el: "#app",
-    render: c => c(login),
+    render: c => c(App),
     router
 })
--- webpack/webpack_router/src/main/Account.vue
+++ webpack/webpack_router/src/main/Account.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>account组件</h1>
+        <router-link to="/account/login">登录</router-link>
+        <router-link to="/account/register">注册</router-link>
+        <div><router-view /></div>
     </div>
 </template>
 
