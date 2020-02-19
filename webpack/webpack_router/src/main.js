1bef3c1a485ed7f74d65321500ac8935794821d8
USER-20010101RH\Administrator <748840808@qq.com>
2020-02-19 18:00:08 +0800

    封装router

--- webpack/webpack_router/src/main.js
+++ webpack/webpack_router/src/main.js
@@ -1,31 +1,10 @@
 // 项目JS入口文件
 import Vue from "vue"
 import VueRouter from "vue-router"
-
-// 导入组件
 import App from "./App.vue"
-import account from "./main/Account.vue"
-import goodslist from "./main/GoodsList.vue"
-
-// 导入子组件
-import login from './subcom/login.vue'
-import register from './subcom/register.vue'
-
+import router from "./router.js"
 Vue.use(VueRouter)
 
-const router = new VueRouter({
-    routes: [
-        {
-            path: '/account',
-            component: account,
-            children: [
-                { path: 'login', component: login },
-                { path: 'register', component: register }
-            ]
-        },
-        { path: '/goodslist', component: goodslist }
-    ]
-})
 
 var vm = new Vue({
     el: "#app",
