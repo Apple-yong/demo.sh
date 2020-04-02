3b76e9f3218d2d55d7f8b6108d878f756bf8d7a9
Apple-yong <748840808@qq.com>
2020-04-02 19:02:42 +0800

    header暂存

---
+++ vue/vue-todomvc/src/components/todos-header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <h1>todos</h1>
+    <input class="new-todo" placeholder="What needs to be done?">
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .header{
+    margin-top: 50px;
+    h1{
+      width: 100%;
+      font-size: 100px;
+      font-weight: 100;
+      text-align: center;
+      color: rgba(175, 47, 47, 0.15);
+    }
+    .new-todo {
+      width: 474px;
+      height: 28px;
+      padding: 16px 16px 16px 60px;
+      border: none;
+      background: rgba(255, 255, 255, 0.925);
+      box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+    }
+  }
+</style>
