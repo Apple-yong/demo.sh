f1be7e6a8032bf056049af10c05f34b14941eceb
Apple-yong <748840808@qq.com>
2020-11-24 15:51:54 +0800

    promise加载图片

--- js_practice/手写jquery/jquery_demo.js
+++ js_practice/手写jquery/jquery_demo.js
@@ -28,3 +28,27 @@ class jQuery {
 jQuery.prototype.dialog = function (info) {
     alert(info)
 }
+
+
+// promise加载图片
+function loadImg(src) {  
+    return new Promise((resolve, reject) => {
+        const img = document.createElement('img')
+        img.onload = () => {
+            resolve(img)
+        }
+        img.onerror = () => {
+            const err = new Error(`图片加载失败 ${src}`)
+            reject(err)
+        }
+        img.src = src
+    });
+}
+
+const url = "https://upload.jianshu.io/users/upload_avatars/4820992/9165dbb8-9df8-467a-af97-773c0e0a2d05.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240"
+loadImg(url).then(img =>{
+    console.log(img.width)
+    return img
+}).then(img =>{
+    console.log(img.height)
+}).catch(ex => console.log(ex))
