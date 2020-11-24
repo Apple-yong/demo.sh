ef048492f6ab62d91110aac6aef80e11a4e1a89e
Apple-yong <748840808@qq.com>
2020-11-24 18:33:00 +0800

    promise运行机制

--- js_practice/手写jquery/jquery_demo.js
+++ js_practice/手写jquery/jquery_demo.js
@@ -78,3 +78,33 @@ function fn1(a,b,c) {
 const fn2 = fn1.bind1({x:100}, 10, 20, 30)
 const res = fn2()
 console.log(res);
+
+
+// Promise执行案例
+Promise.resolve().then(() =>{
+    console.log('1');
+}).catch(() => {
+    console.log('2');
+}).then(() => {
+    console.log('3');
+})
+
+
+Promise.resolve().then(() =>{
+    console.log('1');
+    throw new Error("error1")
+}).catch(() => {
+    console.log('2');
+}).then(() => {
+    console.log('3');
+})
+
+
+Promise.resolve().then(() =>{
+    console.log('1');
+    throw new Error("error1")
+}).catch(() => {
+    console.log('2');
+}).catch(() => {
+    console.log('3');
+})
