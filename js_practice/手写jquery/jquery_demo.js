be5a6c5c9caed05b55b790a186464aa3ef726d03
Apple-yong <748840808@qq.com>
2020-11-25 18:28:09 +0800

    async/await

--- js_practice/手写jquery/jquery_demo.js
+++ js_practice/手写jquery/jquery_demo.js
@@ -108,3 +108,42 @@ Promise.resolve().then(() =>{
 }).catch(() => {
     console.log('3');
 })
+
+// 执行async函数，返回的是Promise对象
+async function fn1() {  
+    return 100
+}
+const res1 = fn1()
+console.log(res1);
+
+// await相当于Promise的then
+(async function () {  
+    const p1 = Promise.resolve(300)
+    const data = await p1    // await相当于Promise的then，resolved才会执行
+    console.log(data);
+})()
+
+(async function () {  
+    const data = await 200    // await相当于Promise的then，resolved才会执行
+    console.log(data);
+})()
+
+
+async function fn1() {  
+    return Promise.resolve(200)
+}
+(async function () {  
+    const data = await fn1()    
+    console.log(data);
+})()
+
+// try...catch可捕获异常，代替了Promise的catch
+(async function () {  
+    const p1 = Promise.reject("err1")
+    try {
+        const res = await p4
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+})()
