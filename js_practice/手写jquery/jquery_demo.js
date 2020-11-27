72fa02d65f93fd19455f13060474534290d3e031
Apple-yong <748840808@qq.com>
2020-11-27 16:11:05 +0800

    通用事件绑定函数

--- js_practice/手写jquery/jquery_demo.js
+++ js_practice/手写jquery/jquery_demo.js
@@ -289,3 +289,25 @@ const pLength = pList.length
 for (let i = 0; i < pLength; i++) {
     // 缓存length ,只进行一次DOM查询 
 }
+
+
+
+// 事件绑定
+function bindEvent(elem, type, selector, fn) {  
+    if(fn == null){
+        fn = selector
+        selector = null
+    }
+    elem.addEventListener(type, event => {
+        const target = event.target
+        if (selector) {
+            // 代理绑定，绑定无限个
+            if (target.matches(selector)) {
+                fn.call(target, event)
+            }
+        } else {
+            // 普通绑定
+            fn.call(target, event)
+        }
+    })
+}
